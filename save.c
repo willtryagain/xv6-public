25bcc8b267617a9a3ac0f0c2d4c77f7a22e28188
willtryagain <41161981+willtryagain@users.noreply.github.com>
2020-10-30 08:07:18 +0000

    Add files via upload

--- save.c
+++ save.c
@@ -1,68 +1,18 @@
 #include "types.h"
+#include "stat.h"
 #include "user.h"
 #include "fcntl.h"
 
-#define N 100
+int open;
+int fd;
 
-struct test {
-    char name;
-    int number;
-};
-
-void
-save(void)
+int main (void)
 {
-    int fd;
-    struct test t;
-    t.name = 's';
-    t.number = 1;
 
-    fd = open("backup", O_CREATE | O_RDWR);
-    if(fd >= 0) {
-        printf(1, "ok: create backup file succeed\n");
-    } else {
-        printf(1, "error: create backup file failed\n");
-        exit();
-    }
+    printf(1,"My Name\n");
+    fd = open("/home/kyle/Desktop/Projects/Assignment 2/xv6/Tom.txt", O_RDWR | O_CREATE);
 
-    int size = sizeof(t);
-    if(write(fd, &t, size) != size){
-        printf(1, "error: write to backup file failed\n");
-        exit();
-    }
-    printf(1, "write ok\n");
+    write(fd, "1 2 3 4", 7);
     close(fd);
-}
 
-void
-load(void)
-{
-    int fd;
-    struct test t;
-
-    fd = open("backup", O_RDONLY);
-    if(fd >= 0) {
-        printf(1, "ok: read backup file succeed\n");
-    } else {
-        printf(1, "error: read backup file failed\n");
-        exit();
-    }
-
-    int size = sizeof(t);
-    if(read(fd, &t, size) != size){
-        printf(1, "error: read from backup file failed\n");
-        exit();
-    }
-    printf(1, "file contents name %c and number %d", t.name, t.number);
-    printf(1, "read ok\n");
-    close(fd);
-}
-
-int
-main(void)
-{
-    save();
-    load();
-
-    exit();
 }
